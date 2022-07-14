a4f75a443b8cbf296491c5585913575489b8b762
bhargav27b <bhargav@sketchbrahma.com>
2022-07-14 23:37:18 +0530

    updated script

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-"use strict"
-
 var submitBtn = document.querySelector('.submitBtn');
-
 
 submitBtn.addEventListener("click", function(e) {
     e.preventDefault();    
-
     var newTaskValue = document.querySelector('.inputText').value;
-    if(newTaskValue) {        
-
-        document.querySelector('.error').style.display = "none";
-
-        var lists = document.querySelector('ul');
+    var error = document.querySelector('.error');
+    
+    if(newTaskValue) {                
+        error.style.display = "none";
+        
         var newList = document.createElement('li');
-
         var editableTask = document.createElement('textarea');
         editableTask.setAttribute("disabled", "true");
         editableTask.value = newTaskValue;
@@ -21,24 +16,22 @@ submitBtn.addEventListener("click", function(e) {
         var removeBtn = document.createElement('span');
         removeBtn.className = "remove";
         removeBtn.textContent = "Remove";
-        removeBtn.addEventListener("click", remove, false);
+        removeBtn.onclick = remove;
 
         var editBtn = document.createElement('span');
         editBtn.className = "edit";
         editBtn.textContent = "edit";
-        editBtn.addEventListener("click", edit, false);
+        editBtn.onclick = edit;
 
-        lists.appendChild(newList);
-        newList.appendChild(editableTask);
-        editableTask.insertAdjacentElement("afterend", editBtn);
-        editBtn.insertAdjacentElement("afterend", removeBtn);                
-
-    } else {
-        document.querySelector('.error').style.display = "block";
+        document.querySelector('ul')
+        .appendChild(newList).appendChild(editableTask)
+        .insertAdjacentElement("afterend", editBtn).insertAdjacentElement("afterend", removeBtn);                     
+    } 
+    else {
+        error.style.display = "block";
     }    
 
-    document.querySelector('.inputText').value = "";    
-    
+    document.querySelector('.inputText').value = "";        
     return false;
 });
 
@@ -57,5 +50,4 @@ function edit(e) {
         inputTask.setAttribute("disabled", "true");
         e.target.textContent = "Edit";
     }
-
 }
